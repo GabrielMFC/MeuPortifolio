bd0582a1aeb6575631968c74cc2582e365ca0c05
Gabriel Matias <ggezcom991gh@gmail.com>
2023-12-14 16:19:16 -0300

    Tentando arrumar o bug

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let fecharopcao = 0
         let projetos = document.getElementById("projetos")
         let sobremim = document.getElementById("sobremim")
 
-       let verificar = localStorage.getItem("verificar")
+       var verificar = localStorage.getItem("verificar")
 
         document.getElementById("opcao").addEventListener("click", function abriropcoes() {
             if(fecharopcao == 0){
