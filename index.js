4f176843a1ffd885c92b5469e48ce05cd8cf4781
Gabriel Matias <ggezcom991gh@gmail.com>
2023-12-16 18:24:10 -0300

    Deixando responsivo e criando a parte de habilidades

--- index.js
+++ index.js
@@ -3,10 +3,7 @@ let fecharopcao = 0
         let opcaoaberta = document.getElementById("opcaoaberta")
 
         let menu = document.getElementById("menu")
-        let habilidades = document.getElementById("habilidades")
         let contato = document.getElementById("contato")
-        let projetos = document.getElementById("projetos")
-        let sobremim = document.getElementById("sobremim")
 
        var verificar = 0
 
@@ -15,16 +12,12 @@ let fecharopcao = 0
             fecharopcao = 1
             opcaoaberta.classList.add("abrir")
             document.getElementById("menu").innerHTML = "Menu"
-            document.getElementById("habilidades").innerHTML = "Habilidades"
-            document.getElementById("projetos").innerHTML = "Projetos"
-            document.getElementById("sobremim").innerHTML = "Sobre mim"
+            document.getElementById("contato").innerHTML = "Contato"
             }else{
                 opcaoaberta.classList.remove("abrir")
                 fecharopcao = 0
                 document.getElementById("menu").innerHTML = ""
-            document.getElementById("habilidades").innerHTML = ""
-            document.getElementById("projetos").innerHTML = ""
-            document.getElementById("sobremim").innerHTML = ""
+            document.getElementById("contato").innerHTML = ""
             }
         })
         opcao.addEventListener("mouseenter", function rotacionar() {
@@ -43,38 +36,17 @@ let fecharopcao = 0
         }
         
         if(verificar == 1) {
-            habilidades.style.textDecoration = "underline"
-            habilidades.style.textDecorationColor = "purple"
-            habilidades.style.cursor = "default"
-            habilidades.style.color = "purple"
+            contato.style.textDecoration = "underline"
+            contato.style.textDecorationColor = "purple"
+            contato.style.cursor = "default"
+            contato.style.color = "purple"
         }
-
-        if(verificar == 2){
-            projetos.style.textDecoration = "underline"
-            projetos.style.textDecorationColor = "purple"
-            projetos.style.cursor = "default"
-            projetos.style.color = "purple"
-        }
-
-        if(verificar == 3){
-            sobremim.style.textDecoration = "underline"
-            sobremim.style.textDecorationColor = "purple"
-            sobremim.style.cursor = "default"
-            sobremim.style.color = "purple"
-        }
+        
         menu.addEventListener("click", function entrarmenu() {
             document.getElementById("linkmenu").href = "index.html"
             localStorage.setItem("verificar", 0)
         })
-        habilidades.addEventListener("click", function  entrarHabilidades() {
+        contato.addEventListener("click", function  entrarContato() {
         document.getElementById("linkhabilidades").href = "habilidades.html"
         localStorage.setItem("verificar", 1)
-        })
-        projetos.addEventListener("click", function entrarprojetos() {
-            document.getElementById("linkprojetos").href = "projetos.html"
-            localStorage.setItem("verificar", 2)
-        })
-        sobremim.addEventListener("click", function entrarsobremim () {
-            document.getElementById("linksobremim").href = "sobremim.html"
-            localStorage.setItem("verificar", 3)
         })
